e0a195edc7938655e41fefaba617bdffb730b8fd
Kevin Duong <kevduong@win2.cis.ksu.edu>
2021-09-11 11:32:54 -0500

    buffer fixed\

---
+++ DDPG Example Implementation/buffer.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pickle
+
+# Expects tuples of (state, next_state, action, reward, done)
+class ReplayBuffer(object):
+    """Buffer to store tuples of experience replay"""
+    
+    def __init__(self, algo, max_size=1000000):
+        """
+        Args:
+            max_size (int): total amount of tuples to store
+        """
+        self.algo = algo
+        
+        self.storage = []
+        self.max_size = max_size
+        self.ptr = 0
+
+    def add(self, data):
+        """Add experience tuples to buffer
+        
+        Args:
+            data (tuple): experience replay tuple
+        """
+        
+        if len(self.storage) == self.max_size:
+            self.storage[int(self.ptr)] = data
+            self.ptr = (self.ptr + 1) % self.max_size
+        else:
+            self.storage.append(data)
+            
+    def save(self, name='ReplayBuff'):
+        '''Save replay buffer for live trading'''
+        self.algo.ObjectStore.Save(name, str(self.storage))
+        self.algo.Log("{} - Saving Replay Buffer!: {}".format(self.algo.Time, len(self.storage)))
+    
+    def load(self, name='ReplayBuff'):
+        '''Save replay buffer for live trading'''
+        self.storage = eval(self.algo.ObjectStore.ReadBytes("key"))
+        self.algo.Log("{} - Loading Replay Buffer!: {}".format(self.algo.Time, len(self.storage)))
+
+    def sample(self, batch_size):
+        """Samples a random amount of experiences from buffer of batch size
+        
+        Args:
+            batch_size (int): size of sample
+        """
+        
+        ind = np.random.randint(0, len(self.storage), size=batch_size)
+        states, actions, next_states, rewards, dones = [], [], [], [], []
+
+        for i in ind: 
+            s, a, s_, r, d = self.storage[i]
+            states.append(np.array(s, copy=False))
+            actions.append(np.array(a, copy=False))
+            next_states.append(np.array(s_, copy=False))
+            rewards.append(np.array(r, copy=False))
+            dones.append(np.array(d, copy=False))
+
+        return np.array(states), np.array(actions), np.array(next_states), np.array(rewards).reshape(-1, 1), np.array(dones).reshape(-1, 1)
